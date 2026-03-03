840472cb56fcbe5673ec4b9bc8364f5f3b5d4983
unknown <mariayunda@yandex.ru>
2026-03-03 18:51:29 +0300

    Final Project

--- study_project/settings.py
+++ study_project/settings.py
@@ -97,8 +97,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'study_app' / 'static',
+    BASE_DIR  / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
